88f6ff9f15edffe4e67833b12f2c210a1813c729
liujiefei <674361437@qq.com>
2018-05-20 10:35:12 +0800

    edit day1

--- day01/while.py
+++ day01/while.py
@@ -1,12 +1,12 @@
 # Author:Alex Li
-'''
+
 count = 0
 while True:
     print("count:",count)
     count = count +1  #count +=1
     if count == 1000:
         break
-'''
+
 '''
 for i in range(0,10):
     if i <3:
@@ -15,10 +15,11 @@ for i in range(0,10):
         continue
     print("hehe...")
 '''
-
-for i in range(10):
+'''
+for i in range(0,10,3):
     print('----------',i)
     for j in range(10):
         print(j)
         if j >5:
             break
+'''

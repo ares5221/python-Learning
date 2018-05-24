28dd8d8ac4832684997d2bac9fbee50bebf8f3ea
liujiefei <674361437@qq.com>
2018-05-25 00:38:00 +0800

    file read

---
+++ day03/FileTest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+'''
+打开文件的模式有：
+r，只读模式（默认）。
+w，只写模式。【不可读；不存在则创建；存在则删除内容；】
+a，追加模式。【可读；   不存在则创建；存在则只追加内容；】
+"+" 表示可以同时读写某个文件
+r+，可读写文件。【可读；可写；可追加】
+w+，写读
+a+，同a
+"U"表示在读取时，可以将 \r \n \r\n自动转换成 \n （与 r 或 r+ 模式同使用）
+rU
+r+U
+"b"表示处理二进制文件（如：FTP发送上传ISO镜像文件，linux可忽略，windows处理二进制文件时需标注）
+rb
+wb
+ab
+'''
+f = open('song.txt',encoding = "utf-8")
+#first_line = f.readline()
+#print(first_line)
+#print(f.read())
+'''
+f = open('geci','',encoding='utf-8')
+f.write("大王饶命\n")
+f.write("新亭侯")
+'''
+
+#高效率的读文件方式，变成迭代器，比readline效率高许多，reanline会读一行，内存保存一行，最后撑爆内存
+count = 0
+for line in f:
+    if count == 9:
+        print("10hang")
+        count +=1
+        continue
+    print(line.strip())
+    count += 1
--- day03/SetTest.py
+++ day03/SetTest.py
@@ -3,15 +3,14 @@
 list_1 = [1,2,3,4,5]
 list_1 = set(list_1)
 print(list_1,type(list_1))
-#集合是无序的，主要用来去重以及测试两组数据之间的交并补关系
+#集合是是一个无序不重复元素集，主要用来去重以及测试两组数据之间的交并补关系
 list_2 = set([3,5,9,10])
 print(list_1,list_2)
 #交集
 print(list_1.intersection(list_2))
 #并集
 print(list_1.union(list_2))
-#补集
-print()
+
 #差集
 print(list_1.difference(list_2))
 #对称差集
@@ -20,36 +19,22 @@ print(list_1.symmetric_difference(list_2))
 print(list_1.issubset(list_2))
 #父集
 print(list_1.issuperset(list_2))
+
+#print(list_1.clear())
+print(list_1.discard(2))#如果在 set “s”中存在元素 x, 则删除
 print("-----------------------------------")
 s = set([3, 5, 9, 10])  # 创建一个数值集合
-
 t = set("Hello")  # 创建一个唯一字符的集合
-
 a = t | s  # t 和 s的并集
-
 b = t & s  # t 和 s的交集
-
 print(t - s)  # 求差集（项在t中，但不在s中）
-
 d = t ^ s  # 对称差集（项在t或s中，但不会同时出现在二者中）
 
 #基本操作：
-
-t.add('x')  # 添加一项
-
-s.update([10, 37, 42])  # 在s中添加多项
-
-#使用remove()S可以删除一项：
-
-t.remove('H')
-
+t.add('x')  #添加一项
+s.update([10, 37, 42])  #在s中添加多项
+t.remove('H')#使用remove()S可以删除一项：
 len(s)#set的长度
-
-t in s
-#测试x是否是s的成员
-
-t not in s
-#测试x是否不是s的成员
-
-
+t in s #测试x是否是s的成员
+t not in s#测试x是否不是s的成员
 s.copy()#返回set “s”的一个浅复制

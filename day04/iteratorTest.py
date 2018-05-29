b4cdc0cdc1e56ad1feff8029e2c618afd095d3ff
liujiefei <liujiefei@zingfront.com>
2018-05-29 19:49:33 +0800

    day04

---
+++ day04/iteratorTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__mtime__ = '2018/5/29'
+"""
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+'''
+#将列表中数据都加1
+b = []
+for i in a:
+    b.append(i+1)
+print(b)
+
+#迭代器
+a = map(lambda x:x+1,a)
+print(a)
+for i in a:
+    print(i)
+'''
+
+

3ac34fa53a74c755d682de6082db475d86f387a8
liujiefei <liujiefei@zingfront.com>
2018-05-29 20:09:00 +0800

    zhuangshiqi

--- day04/decorator.py
+++ day04/decorator.py
@@ -3,3 +3,18 @@
 """
 __mtime__ = '2018/5/29'
 """
+import time
+def timmer(func):
+    def warpper(*args, **kwargs):
+        start_time = time.time()
+        func()
+        stop_time = time.time()
+        print("The func run time is %s" %(stop_time - start_time))
+    return warpper
+
+@timmer
+def testDecorator():
+    time.sleep(3)
+    print("In the Main method测试装饰器！")
+
+testDecorator()

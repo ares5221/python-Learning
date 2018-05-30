258c9ef893a6d861624bc7641d97dc90bbf604d1
liujiefei <674361437@qq.com>
2018-05-31 00:10:59 +0800

    zhuangshiqi

--- day04/shilizhuangshiqi.py
+++ day04/shilizhuangshiqi.py
@@ -6,17 +6,40 @@ __mtime__ = '2018/5/30'
 import time
 user, passwd = 'liujiefei','123456'
 
-def auth():
-    def wrapper(*args, **kwargs):
-        username = input("UserName: ").strip()
-        password = input("PassWord: ").strip()
-        if username == user and password == pass
+def auth(auth_type):
+    print("auth type :",auth_type)
+    def outer_wrapper(func):
+        def wrapper(*args, **kwargs):
+            print("wrapper func args:",*args,**kwargs)
+            if auth_type == "local":
+                username = input("UserName: ").strip()
+                password = input("PassWord: ").strip()
+                if username == user and password == passwd:
+                    print("\033[32;1mUser has passed authentication\033[0m")
+                    res = func(*args, **kwargs)
+                    print("-----after authentication-----")
+                    return res
+                else:
+                    exit("\033[32;1mInvalid username or password\033[0m")
+            elif auth_type == "ldap":
+                print("run with ladp")
+
+
+        return wrapper
+    return outer_wrapper
 
 def index():
     print("Welcom to index page")
-@auth
+@auth(auth_type = "local")
 def home():
     print("welcome to home page")
-@auth
-def bbs():
+    return "from home"
+@auth(auth_type = "ldap")
+def bbs(auth_type = "ldap"):
     print("welcome to bbs page")
+
+
+index()
+#home()
+print(home())
+bbs()
